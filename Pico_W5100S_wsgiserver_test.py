259b828e4830e63edb93d7e49f8d7e2bf3cf2bfc
Bongjun Hur <bj@twarelab.com>
2021-08-20 11:06:39 +0900

    web server add

--- Pico_W5100S_wsgiserver_test.py
+++ Pico_W5100S_wsgiserver_test.py
@@ -31,6 +31,7 @@ print("Wiznet5k Web Server Test (DHCP)")
 # Setup your network configuration below
 # random MAC, later should change this value on your vendor ID
 MY_MAC = (0x00, 0x01, 0x02, 0x03, 0x04, 0x05)
+# # Set manual network configuration
 # IP_ADDRESS = (192, 168, 0, 111)
 # SUBNET_MASK = (255, 255, 0, 0)
 # GATEWAY_ADDRESS = (192, 168, 0, 1)
@@ -57,8 +58,7 @@ ethernetRst.value = True
 # eth = WIZNET5K(spi_bus, cs)
 # Initialize ethernet interface without DHCP
 eth = WIZNET5K(spi_bus, cs, is_dhcp=True, mac=MY_MAC, debug=False)
-
-# # Set network configuration
+# # Set manual network configuration
 # eth.ifconfig = (IP_ADDRESS, SUBNET_MASK, GATEWAY_ADDRESS, DNS_SERVER)
 
 print("Chip Version:", eth.chip)
@@ -74,32 +74,87 @@ requests.set_socket(socket, eth)
 # following functions to be called on specific HTTP GET requests routes
 web_app = WSGIApp()
 
-@web_app.route("/led/<r>/<g>/<b>")
-def led_on(request, r, g, b):  # pylint: disable=unused-argument
-    print("LED handler")
-    # led.fill((int(r), int(g), int(b)))
-    pixels.fill((int(r), int(g), int(b)))
-    pixels.show()
-    return ("200 OK", [], [f"LED set! {r}{g}{b} "])
+html_string = '''
+<!DOCTYPE html>
+<html lang="en">
+<head>
+<meta charset="UTF-8">
+<meta http-equiv="X-UA-Compatible" content="IE=edge">
+<meta name="viewport" content="width=device-width, initial-scale=1.0">
+<title>RaspberryPi Pico Web server - WIZnet W5100S/W5500</title>
+</head>
+<body>
+<div align="center">
+<H1>Welcome! RaspberryPi Pico Web server</H1>
+<h2>Network Information</h2>
+<p>
+Chip Version is $CHIPNAME<br>
+My IP address is $IPADDRESS<br>
+</p>
+<h2>Control LED color</h2>
+<p>
+<label for="colorWell">Color Select: </label><input type="color" value="#000000" id="colorWell"><a href="/led/000000" id="change_color"> -> Change Color</a>
+</p>
+<h2>View file example</h2>
+<p>
+<a href="/code">View code.py</a>
+</p>
+<h2>Go to root</h2>
+<p>
+<a href="/">root page</a>
+</p>
+</div>
+<script>
+var colorWell;
+var defaultColor = "#000000";
+window.addEventListener("load", startup, false);
+function startup() {
+colorWell = document.querySelector("#colorWell");
+colorWell.value = defaultColor;
+colorWell.addEventListener("change", updateAll, false);
+colorWell.select();
+}
+function updateAll(event) {
+var iLen = String(event.target.value).length;
+document.getElementById("change_color").href="/led/" + String(event.target.value).substring(1, iLen);
+}
+</script>
+</body>
+</html>
+'''
 
+html_string = html_string.replace("$CHIPNAME",eth.chip)
+html_string = html_string.replace("$IPADDRESS",eth.pretty_ip(eth.ip_address))
+
+@web_app.route("/led/<color>")
+def led_on(request, color):  # pylint: disable=unused-argument
+    print("LED handler")
+    color_value = int(color, 16)
+    pixels.fill(color_value)
+    pixels.show()
+    html_string_led = html_string.replace("#000000", "#"+color)
+    # return ("200 OK", [], [f"LED set! {r}{g}{b} "])
+    return ("200 OK", [], [html_string_led])
 
 @web_app.route("/")
 def root(request):  # pylint: disable=unused-argument
     print("Root WSGI handler")
-    return ("200 OK", [], ["Root document"])
-
+    # return ("200 OK", [], ["Root document"])
+    return ("200 OK", [], [html_string])
 
 @web_app.route("/large")
 def large(request):  # pylint: disable=unused-argument
     print("Large pattern handler")
     return ("200 OK", [], ["*-.-" * 2000])
 
-
 @web_app.route("/code")
 def code(request):  # pylint: disable=unused-argument
     print("Static file code.py handler")
     return ("200 OK", [], get_static_file("code.py"))
-
+    # response_str = ""
+    # for newbyte in get_static_file("code.py"):
+    #     response_str += str(newbyte)
+    # return ("200 OK", [], [response_str])
 
 @web_app.route("/btc")
 def btc(request):
